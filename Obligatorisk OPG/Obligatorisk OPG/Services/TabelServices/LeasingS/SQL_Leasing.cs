f6851f1b7a882019b5a220b3ba1284a96a1d81da
mrmahir <55674608+mrmahir@users.noreply.github.com>
2023-03-13 12:24:12 +0100

    1.5

--- Obligatorisk OPG/Obligatorisk OPG/Services/TabelServices/LeasingS/SQL_Leasing.cs
+++ Obligatorisk OPG/Obligatorisk OPG/Services/TabelServices/LeasingS/SQL_Leasing.cs
@@ -2,5 +2,149 @@
 {
     public class SQL_Leasing
     {
+<<<<<<< Updated upstream
+=======
+        static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=StudentAccommodation;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        //public static void AddLeasing(Leasing leasing)
+        //{
+        //    string query = $"INSERT into Leasing(LeasingNo, DateFrom, DateTo, StudentNo, RoomNo, DormitoryNumber) Values(@Leasing_No, @Date_From, @Date_to, @Student_No, @Room_No, @Dormitory_Number)";
+        //    using (SqlConnection connection = new SqlConnection(connectionString))
+        //    {
+        //        connection.Open();
+        //        using (SqlCommand command = new SqlCommand(query, connection))
+        //        {
+        //            command.Parameters.AddWithValue("@Leasing_No", leasing.LeasingNo);
+        //            command.Parameters.AddWithValue("@Date_From", leasing.DateFrom);
+        //            command.Parameters.AddWithValue("@Date_To", leasing.DateTo);
+        //            command.Parameters.AddWithValue("@Student_No", leasing)
+                    
+        //            int affectedRows = command.ExecuteNonQuery();
+        //        }
+        //    }
+        //}
+        public static List<Leasing> GetAllLeasings()
+        {
+            List<Leasing> LeasingList = new List<Leasing>();
+            string query = "select * from Leasing";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Leasing leasing = new Leasing();
+                        leasing.LeasingNo = Convert.ToInt32(reader[0]);
+                        leasing.DateFrom = Convert.ToDateTime(reader[1]);
+                        leasing.DateTo = Convert.ToDateTime(reader[2]);
+                        leasing.StudentNo = Convert.ToInt32(reader[3]);
+                        leasing.RoomNo = Convert.ToInt32(reader[4]);
+                        leasing.DormitoryNumber = Convert.ToInt32(reader[5]);
+                        LeasingList.Add(leasing);
+                    }
+                }
+            }
+            return LeasingList;
+        }
+
+        //public static void DeleteLeasing(Leasing leasing)
+        //{
+        //    string query = $" DELETE from Leasing where LeasingNO=@Leasing_No";
+        //    using (SqlConnection connection = new SqlConnection(connectionString))
+        //    {
+        //        connection.Open();
+        //        using (SqlCommand command = new SqlCommand(query, connection))
+        //        {
+        //            command.Parameters.AddWithValue("@Leasing_No", leasing.LeasingNo);
+        //            int affectedRows = command.ExecuteNonQuery();
+        //        }
+        //    }
+        //}
+
+        public static List<Leasing> GetLeasingByLeasingNo(int leasingNo)
+        {
+            List<Leasing> listLeasing = new List<Leasing>();
+
+            string query = $"select * from Leasing WHERE Leasing_No Like '{leasingNo}%' ";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+
+                    while (reader.Read())
+                    {
+                        Leasing leasing = new Leasing();
+                        leasing.LeasingNo = Convert.ToInt32(reader[0]);
+                        leasing.DateFrom = Convert.ToDateTime(reader[1]);
+                        leasing.DateTo = Convert.ToDateTime(reader[2]);
+                        leasing.StudentNo= Convert.ToInt32(reader[3]);
+                        leasing.RoomNo= Convert.ToInt32(reader[4]);
+                        leasing.DormitoryNumber= Convert.ToInt32(reader[5]);
+
+                        listLeasing.Add(leasing);
+                    }
+                    return listLeasing;
+                }
+            }
+        }
+
+        //public static List<Actor_Movies> GetAllActorsAndTheirMovies()
+        //{
+        //    // prepare a list
+        //    List<Actor_Movies> listActor_Movies = new List<Actor_Movies>();
+
+        //    // define the sql query
+        //    string query = "SELECT Actor.name, Actor.country, Movie.title, Movie.Year  from Movie, Actor WHERE Movie.actorid = Actor.Id";
+
+        //    using (SqlConnection connection = new SqlConnection(connectionString))
+        //    {
+        //        connection.Open();
+        //        SqlCommand command = new SqlCommand(query, connection);
+        //        using (SqlDataReader reader = command.ExecuteReader())
+        //        {
+
+        //            while (reader.Read())
+        //            {
+        //                Actor_Movies actor_movie = new Actor_Movies();
+        //                actor_movie.Name = Convert.ToString(reader[0]);
+        //                actor_movie.Country = Convert.ToString(reader[1]);
+        //                actor_movie.Title = Convert.ToString(reader[2]);
+        //                actor_movie.Year = Convert.ToInt32(reader[3]);
+
+        //                listActor_Movies.Add(actor_movie);
+        //            }
+        //            return listActor_Movies;
+        //        }
+        //    }
+        //}
+
+        //public static Actor GetActorById(int actorId)
+        //{
+        //    string query = $"SELECT * From Actor where Id=@aid";
+        //    Actor actor = new Actor();
+        //    using (SqlConnection connection = new SqlConnection(connectionString))
+        //    {
+        //        connection.Open();
+        //        SqlCommand command = new SqlCommand(query, connection);
+        //        command.Parameters.AddWithValue("@aid", actorId);
+        //        using (SqlDataReader reader = command.ExecuteReader())
+        //        {
+        //            while (reader.Read()) // while I am reading row by row
+        //            {
+        //                actor.Id = Convert.ToInt32(reader["Id"]);
+        //                actor.Name = Convert.ToString(reader["Name"]);
+        //                actor.Country = Convert.ToString(reader["Country"]);
+        //                actor.Birth_year = Convert.ToDateTime(reader["Birth_year"]);
+        //                actor.Alive = Convert.ToBoolean(reader["Alive"]);
+        //            }
+        //        }
+        //    }
+        //    return actor;
+        //}
+>>>>>>> Stashed changes
     }
 }
--- Obligatorisk OPG/Obligatorisk OPG/Services/TabelServices/StudentS/SQL_Student.cs
+++ Obligatorisk OPG/Obligatorisk OPG/Services/TabelServices/StudentS/SQL_Student.cs
@@ -2,5 +2,98 @@
 {
     public class SQL_Student
     {
+<<<<<<< Updated upstream
     }
+=======
+        static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=StudentAccommodation;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        private static List<Student> listStudents;
+
+        public static void AddStudent(Student student)
+        {
+            string query = $"INSERT into Student(Id,Name, Adress) Values(@Id, @Name, @Adress)";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", student.Student_No);
+                    command.Parameters.AddWithValue("@Name", student.Name);
+                    command.Parameters.AddWithValue("@Country", student.Address);
+                    int affectedRows = command.ExecuteNonQuery();
+                }
+            }
+        }
+        public static List<Student> GetAllStudents()
+        {
+            List<Student> studentList = new List<Student>();
+            string query = "select * from Student";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Student student = new Student();
+                        student.Student_No = Convert.ToInt32(reader[0]);
+                        student.Name = Convert.ToString(reader[1]);
+                        student.Address = Convert.ToString(reader[2]);
+                        studentList.Add(student);
+                    }
+                }
+            }
+            return studentList;
+        }
+        public static List<Student> GetAllStudentsByAdress(string adress)
+        {
+            // prepare a list
+            List<Student> listActor_Movies = new List<Student>();
+
+            // define the sql query
+            string query = "SELECT Student.name, Student.adress, Student WHERE Adress.id = Student.StudentNo";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+
+                    while (reader.Read())
+                    {
+                        Student student = new Student();
+                        student.Name = Convert.ToString(reader[0]);
+                        student.Address = Convert.ToString(reader[1]);
+
+                        listStudents.Add(student);
+                    }
+                    return listStudents;
+                }
+            }
+        }
+
+        public static Student GetStudentById(int studentId)
+        {
+            string query = $"SELECT * From Student where Id=@aid";
+            Student student = new Student();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@aid", studentId);
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read()) // while I am reading row by row
+                    {
+                        student.Student_No = Convert.ToInt32(reader["Id"]);
+                        student.Name = Convert.ToString(reader["Name"]);
+                        student.Address = Convert.ToString(reader["Adress"]);
+                    }
+                }
+            }
+            return student;
+        }
+    } 
+>>>>>>> Stashed changes
 }
